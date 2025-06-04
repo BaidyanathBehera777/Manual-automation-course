e21609c1782e78e918d5fa6268fb3029722e61e8
BaidyanathBehera777 <56757518+BaidyanathBehera777@users.noreply.github.com>
2025-06-04 22:14:57 +0530

    Add files via upload

---
+++ javaprogram.java
@@ -0,0 +1,10 @@
+package myfirstpackage;
+
+public class javaprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("champion of 2025 ipl RCB");
+	}
+
+}
