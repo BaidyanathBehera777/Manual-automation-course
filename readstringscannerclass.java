cd0871c94faed9715333f2f96624fb9b6f888ff1
BaidyanathBehera777 <56757518+BaidyanathBehera777@users.noreply.github.com>
2025-06-18 20:55:53 +0530

    Add files via upload

---
+++ readstringscannerclass.java
@@ -0,0 +1,21 @@
+package string;
+
+import java.util.Scanner;
+
+public class readstringscannerclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter an integer: ");
+        int myInt = sc.nextInt();
+	    System.out.println("Enter a float :");
+	    float myfloat = sc.nextFloat();
+	    System.out.println("Enter a double :");
+	    double mydouble = sc.nextDouble();
+	    System.out.println("Enter a boolean (true/false :)");
+	    boolean mybool = sc.nextBoolean();
+	
+	}
+
+}
