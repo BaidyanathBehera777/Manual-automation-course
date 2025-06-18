e8ed359af4360ec8b870467301983d57e084deea
BaidyanathBehera777 <56757518+BaidyanathBehera777@users.noreply.github.com>
2025-06-18 20:29:22 +0530

    Add files via upload

---
+++ countnumber.java
@@ -0,0 +1,11 @@
+package string;
+
+public class countnumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      String str = "banana";
+      System.out.println(str.codePointCount(1, 3));
+	}
+
+}
